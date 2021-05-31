e835e58dc2734b401f03f8137f727fb7f832ce0f
Kevin Rösch <kevin.roesch@kit.edu>
2021-05-31 12:10:42 +0200

    add functions to force occlusion

--- basic_approaches/basic_img_augmentation.py
+++ basic_approaches/basic_img_augmentation.py
@@ -9,7 +9,7 @@ import json
 from reprint import output
 import random
 import numpy as np
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 from geometric_transformations import *
 from multiprocessing import Lock, Process, Queue, current_process, Value
 from ctypes import c_char_p
@@ -22,6 +22,9 @@ sidewalk_value = 8
 
 # Foreground
 person_value = 24
+
+# Occlusion
+obstacle_values = [13, 14, 15 ,17, 19, 20, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33]
 
 class textcolor:
     HEADER = '\033[95m'
@@ -70,8 +73,8 @@ class AugmentationWorker:
         self.state = Value(c_char_p, b"init")
     def augmentImageInner(self, fg_path_list, bg_path_list, task_id):
         # get a random foreground and background
-        fg_path = random.choice(fg_path_list)  
-        bg_path = random.choice(fg_path_list)
+        fg_path = random.choice(fg_path_list)
+        bg_path = random.choice(bg_path_list)
 
         # Data name chosing
         FGname, BGname = data_name(fg_path, bg_path)
@@ -91,68 +94,69 @@ class AugmentationWorker:
         # Background fliping
         flip_BGimg, flip_BGmask = data_fliper(BGimg, BGmask)
         self.state.value = b"Preprocess objects..."
-        # Object preprocessing 
+        # Object preprocessing
         try:
-            obj_img, obj_mask, x,y,w,h = obj_preprocesser(flip_FGimg, 
-                                                        flip_FGmask, 
-                                                        BGheight, 
-                                                        BGwidth, 
-                                                        person_value, 
-                                                        FGheight, 
+            obj_img, obj_mask, x,y,w,h = obj_preprocesser(flip_FGimg,
+                                                        flip_FGmask,
+                                                        BGheight,
+                                                        BGwidth,
+                                                        person_value,
+                                                        FGheight,
                                                         FGwidth)
         except OSError:
             raise FailedAugmentation()
         self.state.value = b"Find random place..."
         # Random place finding
         try:
-            stand_y, stand_x = random_place_finder(flip_BGmask,
-                                                    ground_value, 
-                                                    sidewalk_value, 
-                                                    road_value, 
-                                                    BGheight, 
-                                                    BGwidth,
-                                                    y,
-                                                    h)
+            if force_occlusion_flag:
+                bottom_pixel_person = force_occlusion(flip_BGmask, obj_mask, ground_value, sidewalk_value, road_value, obstacle_values, y, h, min_occlusion_ratio)
+            else:
+                bottom_pixel_person = random_place_finder(flip_BGmask,
+                                                        ground_value,
+                                                        sidewalk_value,
+                                                        road_value,
+                                                        y,
+                                                        h)
         except IOError:
             raise FailedAugmentation("Could not find any road to place the object on.")
         # Size of person finding
         obj_mask_height = obj_mask.shape[0]; obj_mask_width = obj_mask.shape[1]
-        stand_obj_height, stand_obj_width = person_size_finder(stand_y, 
-                                                                w, 
-                                                                h, 
-                                                                obj_mask_height, 
+        stand_obj_height, stand_obj_width = person_size_finder(bottom_pixel_person[0],
+                                                                w,
+                                                                h,
+                                                                obj_mask_height,
                                                                 obj_mask_width)
         self.state.value = b"Matting.."
-        # Img and mask of object resizing 
+        # Img and mask of object resizing
         # Matting function using
-        resized_obj_img, resized_obj_mask, alpha, smoother_mask, trimap_mask = obj_resizer(obj_img, 
-                                                                                            obj_mask, 
-                                                                                            stand_obj_height, 
-                                                                                            stand_obj_width, 
+        resized_obj_img, resized_obj_mask, alpha, smoother_mask, trimap_mask = obj_resizer(obj_img,
+                                                                                            obj_mask,
+                                                                                            stand_obj_height,
+                                                                                            stand_obj_width,
                                                                                             person_value)
 
-        # Foreground and background preprocessing 
-        fg_bg_img, fg_bg_mask = fg_bg_preprocesser(resized_obj_img, 
-                                                    smoother_mask, 
-                                                    alpha, 
-                                                    flip_BGimg, 
+        # Foreground and background preprocessing
+        fg_bg_img, fg_bg_mask = fg_bg_preprocesser(resized_obj_img,
+                                                    smoother_mask,
+                                                    alpha,
+                                                    flip_BGimg,
                                                     flip_BGmask,
-                                                    stand_y, stand_x, 
-                                                    stand_obj_height, 
-                                                    stand_obj_width, 
-                                                    BGheight, 
+                                                    bottom_pixel_person[0], bottom_pixel_person[1],
+                                                    stand_obj_height,
+                                                    stand_obj_width,
+                                                    BGheight,
                                                     BGwidth,
                                                     person_value)
         self.state.value = b"Saving..."
-        # Data saving 
+        # Data saving
         data_saver(BGname, fg_bg_img, fg_bg_mask, task_id)
 
     def augmentImage(self, task_queue, fg_path_list, bg_path_list):
         while True:
             try:
                 '''
-                    try to get task from the queue. get_nowait() function will 
-                    raise queue.Empty exception if the queue is empty. 
+                    try to get task from the queue. get_nowait() function will
+                    raise queue.Empty exception if the queue is empty.
                     queue(False) function would do the same task also.
                 '''
                 # pull a task number from the queue and try to augment the image
@@ -191,7 +195,7 @@ def data_loader(fg_path,bg_path):
     FGimg = cv2.cvtColor(FGimg, cv2.COLOR_BGR2RGB)
     FGmask = cv2.imread(maskFG_path)
     FGmask = cv2.cvtColor(FGmask, cv2.COLOR_BGR2RGB)
-    
+
     BGimg = cv2.imread(imgBG_path)
     BGimg = cv2.cvtColor(BGimg, cv2.COLOR_BGR2RGB)
     BGmask = cv2.imread(maskBG_path)
@@ -203,10 +207,10 @@ def data_loader(fg_path,bg_path):
 def data_saver(data_name, img, mask, id_data):
     img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
     mask = cv2.cvtColor(mask, cv2.COLOR_RGB2BGR)
-    
+
     img_path = os.path.join(save_directory, 'img', data_name + '_' + str(id_data) + '.jpg')
     mask_path = os.path.join(save_directory, 'mask', data_name + '_' + str(id_data) +'.png')
-    
+
     cv2.imwrite(img_path, img)
     cv2.imwrite(mask_path, mask)
 
@@ -214,7 +218,7 @@ def data_saver(data_name, img, mask, id_data):
 
 def current_id():
     path_list = glob.glob(os.path.join(save_directory, 'mask','*'))
-    
+
     if not path_list:
         current_id = 1
     else:
@@ -239,6 +243,8 @@ if __name__ == '__main__':
     DEFAULT_FG_PATH = os.path.abspath(os.path.join(file_dir, "../basic_approaches/citysc_fgPaths.json"))
     DEFAULT_BG_PATH = os.path.abspath(os.path.join(file_dir, "../basic_approaches/citysc_bgPaths.json"))
     DEFAULT_NUMBER_PROCESSES = 4
+    DEFAULT_OCCLUSION_FLAG = False
+    DEFAULT_MIN_OCCLUSION_RATIO = 0.4
 
     parser = argparse.ArgumentParser(description='Create the augmented dataset for cityscapes.')
     parser.add_argument('--dataset-size', dest='dataset_size', type=int, default=DEFAULT_DATASET_SIZE,
@@ -247,6 +253,10 @@ if __name__ == '__main__':
     parser.add_argument('--fg', dest='fgPaths', default=DEFAULT_FG_PATH, help='Select the json files which where created by the path_creating script.')
     parser.add_argument('--bg', dest='bgPaths', default=DEFAULT_BG_PATH, help='Select the json files which where created by the path_creating script.')
     parser.add_argument('--process', dest='num_processes', default=DEFAULT_NUMBER_PROCESSES, help='Select the number of processes.')
+    parser.add_argument('--force_occlusion', dest='force_occlusion', default=DEFAULT_OCCLUSION_FLAG, type=bool, help='Forces occlusion of pedestrians with objects.')
+    parser.add_argument('--min_occlusion_ratio', dest='min_occlusion_ratio', default=DEFAULT_MIN_OCCLUSION_RATIO, type=float, help='Set the occlusion ratio of the pedestrian.')
+
+
 
     args = parser.parse_args()
     dataset_size = args.dataset_size
@@ -255,6 +265,8 @@ if __name__ == '__main__':
     bgPaths = args.bgPaths
     save_directory = args.output_path
     num_processes = args.num_processes
+    force_occlusion_flag = args.force_occlusion
+    min_occlusion_ratio = args.min_occlusion_ratio
 
     checkForFolder(save_directory)
     fg_path_list = pathReader(fgPaths)
@@ -272,7 +284,7 @@ if __name__ == '__main__':
     # put as many task on the queue as we want to have images in our dataset
     for i in range(dataset_size-id_data):
         task_queue.put(i)
-    
+
     manager = AugmentationWorkerManager(num_processes, task_queue, fg_path_list, bg_path_list)
     while not task_queue.empty:
         pass
